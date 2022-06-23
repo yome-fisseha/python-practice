71f14273968800ee77473b97855d87aa75bc1ed9
Yome Fisseha <yomefisseha203@gmail.com>
2022-06-23 17:33:58 +0300

    grade calculator python program

---
+++ grade.py
@@ -0,0 +1,34 @@
+x=input("insert your grade")
+print("score=",x)
+x=int(x)
+ 
+if x>=90 and x<100:
+    print("grade=A+")
+
+if x>=85 and x<90:
+    print("grade=A")
+    
+if x>=80 and x<85:
+    print("grade=A-")
+    
+if x>=75 and x<80:
+    print("grade=B+")
+    
+if x>=70 and x<75:
+    print("grade=B")
+    
+if x>=65 and x<70:
+    print("grade=B-")
+    
+if x>=60 and x<65:
+    print("grade=C+")
+    
+if x>=50 and x<60:
+    print("grade=C")
+    
+if x>=40 and x<50:
+    print("grade=C-")
+    
+if x<40:
+    print("grade=D")
+    
